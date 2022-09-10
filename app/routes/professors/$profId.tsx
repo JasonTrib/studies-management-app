a31a2318ea48f2066b9177566e83b61e82beb78f
JasonTrib <jasontrib10@gmail.com>
2022-09-10 23:30:19 +0300

    fix unhandled route professors/:id

---
+++ app/routes/professors/$profId.tsx
@@ -0,0 +1,18 @@
+import type { LoaderFunction } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
+import { getProfessor } from "~/DAO/professorDAO.server";
+import { paramToInt } from "~/utils/paramToInt";
+
+export const loader: LoaderFunction = async ({ request, params }) => {
+  const id = paramToInt(params.profId);
+  if (id == null) {
+    throw new Response("Not Found", { status: 404 });
+  }
+
+  const prof = await getProfessor(id);
+  if (!prof) {
+    throw new Response("Not Found", { status: 404 });
+  }
+
+  return redirect(`/profile/${prof.user_id}`);
+};
