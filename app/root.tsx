1ef311293d26e41a31c192d3362ac05ce528ae1a
JasonTrib <jasontrib10@gmail.com>
2022-09-26 01:11:42 +0300

    name app

--- app/root.tsx
+++ app/root.tsx
@@ -16,7 +16,7 @@ export { ErrorBoundary } from "~/components/ErrorBoundary";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "Studies Management App",
+  title: "Unilumnus",
   viewport: "width=device-width,initial-scale=1",
 });
 
